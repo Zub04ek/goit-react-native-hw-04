5f17536699b23904e06ab4ac4cffbcead0b3fcc6
Zub04ek <nastasyz@ukr.net>
2023-06-13 16:29:59 +0300

    added bottom tab navigator

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
-import 'react-native-gesture-handler';
+import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import LoginScreen from "./Screens/auth/LoginScreen";
 import RegistrationScreen from "./Screens/auth/RegistrationScreen";
+import PostsScreen from "./Screens/main/PostsScreen";
+import CreatePostsScreen from "./Screens/main/CreatePostsScreen";
+import ProfileScreen from "./Screens/main/ProfileScreen";
 
 const AuthStack = createStackNavigator();
+const MainTab = createBottomTabNavigator();
 
-export default function App() {
-  return (
-    <NavigationContainer>
+const useRoute = (isAuth) => {
+  if (!isAuth) {
+    return (
       <AuthStack.Navigator initialRouteName="Log in">
         <AuthStack.Screen
           name="Log in"
@@ -25,6 +30,18 @@ export default function App() {
           }}
         />
       </AuthStack.Navigator>
-    </NavigationContainer>
+    );
+  }
+  return (
+    <MainTab.Navigator screenOptions={{ headerShown: false }}>
+      <MainTab.Screen name="Posts" component={PostsScreen} />
+      <MainTab.Screen name="Create" component={CreatePostsScreen} />
+      <MainTab.Screen name="Profile" component={ProfileScreen} />
+    </MainTab.Navigator>
   );
+};
+
+export default function App() {
+  const routing = useRoute({});
+  return <NavigationContainer>{routing}</NavigationContainer>;
 }
