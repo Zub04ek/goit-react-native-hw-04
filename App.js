28d9066f1fb0aee3c3c0dec37bd7528679f6bbba
Zub04ek <nastasyz@ukr.net>
2023-07-27 17:05:03 +0300

    fix router file

--- App.js
+++ App.js
@@ -4,6 +4,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import { useRoute } from "./router";
 
 export default function App() {
-  const routing = useRoute({});
+  const routing = useRoute(null);
   return <NavigationContainer>{routing}</NavigationContainer>;
 }
--- router.js
+++ router.js
@@ -1,59 +1,71 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import LoginScreen from "./Screens/auth/LoginScreen";
 import RegistrationScreen from "./Screens/auth/RegistrationScreen";
-import PostsScreen from "./Screens/main/PostsScreen";
-import CreatePostsScreen from "./Screens/main/CreatePostsScreen";
-import ProfileScreen from "./Screens/main/ProfileScreen";
-
-import { AntDesign } from "@expo/vector-icons";
+import { HomeScreen } from "./Screens/auth/HomeScreen";
 
 const AuthStack = createStackNavigator();
-const MainTab = createBottomTabNavigator();
 
 export const useRoute = (isAuth) => {
-  if (!isAuth) {
-    return (
-      <AuthStack.Navigator initialRouteName="Log in">
-        <AuthStack.Screen
-          name="Log in"
-          component={LoginScreen}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <AuthStack.Screen
-          name="Sign up"
-          component={RegistrationScreen}
-          options={{
-            headerShown: false,
-          }}
-        />
-      </AuthStack.Navigator>
-    );
-  }
+  // if (!isAuth) {
   return (
-    <MainTab.Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false }} >
-      <MainTab.Screen
+    <AuthStack.Navigator initialRouteName="Log in">
+      <AuthStack.Screen
+        name="Log in"
+        component={LoginScreen}
         options={{
-          tabBarIcon: ({ focused, size, color }) => (
-            <AntDesign name="appstore-o" size={size} color={color} />
-          ),
+          headerShown: false,
         }}
-        name="Posts"
-        component={PostsScreen}
       />
-      <MainTab.Screen options={{
-          tabBarIcon: ({ focused, size, color }) => (
-            <AntDesign name="pluscircle" size={size} color={color} />
-          ),
-        }} name="Create" component={CreatePostsScreen} />
-      <MainTab.Screen options={{
-          tabBarIcon: ({ focused, size, color }) => (
-            <AntDesign name="user" size={size} color={color} />
-          ),
-        }} name="Profile" component={ProfileScreen} />
-    </MainTab.Navigator>
+      <AuthStack.Screen
+        name="Sign up"
+        component={RegistrationScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <AuthStack.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </AuthStack.Navigator>
   );
+  // }
+  // return (
+  //   <MainTab.Navigator screenOptions={{ tabBarShowLabel: false }}>
+  //     <MainTab.Screen
+  //       options={{
+  //         tabBarIcon: ({ focused, size, color }) => (
+  //           <AntDesign name="appstore-o" size={size} color={color} />
+  //         ),
+  //         title: "Posts",
+  //       }}
+  //       name="Posts"
+  //       component={PostsScreen}
+  //     />
+  //     <MainTab.Screen
+  //       options={{
+  //         tabBarIcon: ({ focused, size, color }) => (
+  //           <AntDesign name="pluscircle" size={size} color={color} />
+  //         ),
+  //         title: "Create post",
+  //       }}
+  //       name="Create"
+  //       component={CreatePostsScreen}
+  //     />
+  //     <MainTab.Screen
+  //       options={{
+  //         tabBarIcon: ({ focused, size, color }) => (
+  //           <AntDesign name="user" size={size} color={color} />
+  //         ),
+  //         title: "Profile",
+  //       }}
+  //       name="Profile"
+  //       component={ProfileScreen}
+  //     />
+  //   </MainTab.Navigator>
+  // );
 };
