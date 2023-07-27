317c21507b22650ffe3a939ae1771eba26c53d1a
Zub04ek <nastasyz@ukr.net>
2023-07-27 17:11:06 +0300

    setted & styled main screens

--- Screens/main/PostsScreen.js
+++ Screens/main/PostsScreen.js
@@ -1,10 +1,41 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, FlatList, Dimensions } from "react-native";
+import { useFonts } from "expo-font";
+import { Card } from "../../components/Card";
+import { useRoute } from "@react-navigation/native";
+import { postsData } from "../../data/posts";
 
 const PostsScreen = () => {
+  const [posts, setPosts] = useState(postsData);
+
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("../../assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Bold": require("../../assets/fonts/Roboto-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>PostsScreen</Text>
+      <FlatList
+        style={styles.list}
+        data={posts}
+        renderItem={({ item }) => {
+          return posts.length > 0 && <Card item={item} />;
+        }}
+        keyExtractor={(item) => item.id}
+        ListHeaderComponent={() => (
+          <View style={styles.userCard}>
+            <View style={styles.userPhoto}></View>
+            <View style={styles.userInfo}>
+              <Text style={styles.userName}>User name</Text>
+              <Text style={styles.userEmail}>User email</Text>
+            </View>
+          </View>
+        )}
+      />
     </View>
   );
 };
@@ -12,8 +43,35 @@ const PostsScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    backgroundColor: "#FFFFFF",
+    paddingTop: 32,
+  },
+  userCard: {
+    flexDirection: "row",
     alignItems: "center",
+    gap: 8,
+    paddingLeft: 16,
+    paddingRight: 16,
+    marginBottom: 32,
+  },
+  userPhoto: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#E5E5E5",
+    borderRadius: 16,
+  },
+  userName: {
+    fontFamily: "Roboto-Bold",
+    fontWeight: "700",
+    fontSize: 13,
+    lineHeight: 15,
+    color: "#212121",
+  },
+  userEmail: {
+    fontFamily: "Roboto-Regular",
+    fontSize: 11,
+    lineHeight: 13,
+    color: "rgba(33, 33, 33, 0.8)",
   },
 });
 
